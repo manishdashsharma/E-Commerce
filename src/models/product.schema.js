28d82d6a9dd8e4e2db60812dee1dc29f5095e6c5
Manish Dash Sharma <60460877+manishdashsharma@users.noreply.github.com>
2023-04-26 16:51:10 +0000

    Product Schema and fixed User Schema

---
+++ src/models/product.schema.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: ["true", "please provide a product name"],
+        trim: true,
+        maxLength: [120, "product name should not be max than 120 chars"]
+    },
+    price: {
+        type: Number,
+        required: ["true", "please provide a product price"],
+        maxLength: [5, "product name should not be max than 5 chars"]
+    },
+    description: {
+        type: String
+    },
+    photos: [
+        {
+            sucure_url: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    stock: {
+        type: String,
+        default: 0
+    },
+    sold: {
+        type: String,
+        default: 0
+    },
+    collectionId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Collection"
+    }
+},{timestamps: true})
+
+export default mongoose.model("Product",productSchema)
