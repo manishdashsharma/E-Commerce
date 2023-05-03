2e1064f4a8ac4c1f4cccba58459269543607c58e
Manish Dash Sharma <60460877+manishdashsharma@users.noreply.github.com>
2023-05-03 04:29:35 +0000

    Coupon Route

--- src/routes/coupon.route.js
+++ src/routes/coupon.route.js
@@ -6,6 +6,6 @@ import AuthRoles from "../utils/authRole.js"
 const router = Router()
 
 router.post('/', isLoggedIn , authorize(AuthRoles.ADMIN , AuthRoles.MODERATOR) , createCoupon)
-router.put("/:id", isLoggedIn , authorize(AuthRoles.ADMIN) , updateCoupon)
-router.delete("/:id", isLoggedIn, authorize(AuthRoles.ADMIN) , deleteCoupon)
-router.get("/all-coupon", isLoggedIn, authorize(AuthRoles.USER) , getAllCoupons)
+router.put("/:id", isLoggedIn , authorize(AuthRoles.ADMIN , AuthRoles.MODERATOR) , updateCoupon)
+router.delete("/:id", isLoggedIn, authorize(AuthRoles.ADMIN , AuthRoles.MODERATOR) , deleteCoupon)
+router.get("/", isLoggedIn, authorize(AuthRoles.ADMIN , AuthRoles.MODERATOR) , getAllCoupons)
