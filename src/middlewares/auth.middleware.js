cea595bd3432bfda9204e7a34b30bf0c2cae2f2b
Manish <mdashsharma95@gmail.com>
2023-06-16 16:47:24 +0530

    fixed auth.middleware token

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -5,26 +5,25 @@ import config from "../config/index.js"
 import CustomError from "../services/CustomError.js"
 
 
-export const isLoggedIn = asyncHandler( async( req, res, next ) =>{
+export const isLoggedIn = asyncHandler(async (req, res, next) => {
     let token;
-
-    if (req.cookies.token || (req.header.authorization && req.header.authorization.startsWith("Bearer") )){
-        token = req.cookies.token || req.header.authorization.split(" ")[1]
+  
+    if (req.cookies.token || (req.headers.authorization && req.headers.authorization.startsWith("Bearer"))) {
+      token = req.cookies.token ? req.cookies.token : req.headers.authorization.split(" ")[1];
     }
-
-    if(!token){
-        throw new CustomError("Not authorized to access this resource",401)
+    
+    if (!token) {
+      throw new CustomError("Not authorized to access this resource", 401);
     }
-
+    
     try {
-        const decodedJWTPayload = JWT.verify(token,config.JWT_SECRET);
-
-        req.user = await User.findById(decodedJWTPayload._id , "name email role")
-        next()
+      const decodedJWTPayload = JWT.verify(token, config.JWT_SECRET);
+      req.user = await User.findById(decodedJWTPayload._id, "name email role");
+      next();
     } catch (error) {
-        throw new CustomError("Not authorized to access this resource",401)
+      throw new CustomError("Not authorized to access this resource", 401);
     }
-})
+  });
 
 export const authorize = (...requiredRoles) => asyncHandler( async (req, res, next) => {
     if (!requiredRoles.includes(req.user.role)) {
